220f0f8648e68bdd7ccb7645de7d52b9c7fb5c75
Tomasz <tomasz@stechly.com>
2014-01-14 20:40:06 -0700

    Add websocket Logger

--- tester/Main.cs
+++ tester/Main.cs
@@ -1,4 +1,5 @@
 using System;
+using weblog;
 
 namespace weblogclienttester
 {
@@ -6,8 +7,10 @@ namespace weblogclienttester
 	{
 		public static void Main (string[] args)
 		{
-			new weblog.Logger();
-			Console.WriteLine ("Hello World!");
+			Logger logger = new weblog.Logger("ec2-174-129-123-237.compute-1.amazonaws.com:9000", "93c5a127-e2a4-42cc-9cc6-cf17fdac8a7f");
+		
+			Console.WriteLine ("Press Enter key to stop...");
+			Console.ReadLine();
 		}
 	}
 }
--- weblog-ng-client-dotnet/Logger.cs
+++ weblog-ng-client-dotnet/Logger.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Net.Sockets;
+using System.Net;
+using WebSocket4Net;
+using SuperSocket.ClientEngine;
 
 namespace weblog
 {
@@ -6,11 +10,42 @@ namespace weblog
 	{
 		private String id;
 		private String apiUrl;
+		private Socket serverSocket;
+		private WebSocket websocket;
 		
-		
-		public Logger ()
+		public Logger (String apiHost, String apiKey)
 		{
+			Console.WriteLine("Weblogng: initializing...");
+			apiUrl = "ws://"+apiHost+"/log/ws";
+			websocket = new WebSocket (apiUrl);
+			
+			websocket.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs> (websocket_Error);
+			websocket.Opened += new EventHandler (websocket_Opened);
+			websocket.Closed += new EventHandler (websocket_Closed);
+			websocket.MessageReceived += new EventHandler<MessageReceivedEventArgs> (websocket_MessageReceived);
+			websocket.Open ();
+			Console.WriteLine("Weblogng: Websocket version:"+websocket.Version);
 		}
+		
+		private void websocket_Opened (object sender, EventArgs e)
+		{
+			Console.WriteLine("Weblogng: Connected");
+		}
+		
+		private void websocket_Error(object sender, ErrorEventArgs e) {
+			Console.WriteLine("Weblogng: Error ");
+		}
+
+		private static void websocket_MessageReceived(object sender, MessageReceivedEventArgs e) {
+			Console.WriteLine("Weblogng: Message "+e.Message);
+		}
+		
+		private static void websocket_Closed(object sender, System.EventArgs e) {
+			Console.WriteLine("Weblogng: Connection closed");
+		}
+		
 	}
+	
+		
 }
 
