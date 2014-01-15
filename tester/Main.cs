aa45db3cf105514239515a193535d1be537dbed2
Tomasz <tomasz@stechly.com>
2014-01-14 19:26:34 -0700

    Initial checkin of solution. Includes library and simple console test app

---
+++ tester/Main.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace weblogclienttester
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			new weblog.Logger();
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ weblog-ng-client-dotnet/Logger.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace weblog
+{
+	public class Logger
+	{
+		private String id;
+		private String apiUrl;
+		
+		
+		public Logger ()
+		{
+		}
+	}
+}
+
